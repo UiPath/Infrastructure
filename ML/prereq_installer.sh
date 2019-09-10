afbf9c372245cf238b8c7d8d9c5198a6048d6b5b
hteo1337 <hteo1337@gmail.com>
2019-09-10 14:57:23 +0300

    main installer

---
+++ ML/prereq_installer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#checking  OS
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+
+if [ "$distribution" = "centos7" ] || [ "$distribution" = "rhel7"* ]; then
+    curl -fsSL https://raw.githubusercontent.com/UiPath/Infrastructure/master/ML/ml_prereqs_rhel7.sh | sh
+elif  [[ "$distribution"  = "ubuntu"* ]]; then
+    curl -fsSL https://raw.githubusercontent.com/UiPath/Infrastructure/master/ML/ml_prereqs_ubuntu16.sh | sh
+else
+   echo  "local OS is not supported"
+   exit 1
+fi
+
+
