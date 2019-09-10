84fb2a4ff3f6fbe9b682607d667878b5016ef060
hteo1337 <hteo1337@gmail.com>
2019-09-10 15:25:20 +0300

    Update prereq_installer.sh

--- ML/prereq_installer.sh
+++ ML/prereq_installer.sh
@@ -3,7 +3,7 @@
 #checking  OS
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 
-if [ "$distribution" = "centos7" ] || [ "$distribution" = "rhel7"* ]; then
+if [ [ "$distribution" = "rhel7"* ]]; then
     curl -fsSL https://raw.githubusercontent.com/UiPath/Infrastructure/master/ML/ml_prereqs_rhel7.sh | sh
 elif  [[ "$distribution"  = "ubuntu"* ]]; then
     curl -fsSL https://raw.githubusercontent.com/UiPath/Infrastructure/master/ML/ml_prereqs_ubuntu16.sh | sh
