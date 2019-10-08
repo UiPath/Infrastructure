4cecfa1f46b049aaa4060d786c503e67b9c5ed2f
hteo1337 <hteo1337@gmail.com>
2019-10-08 23:15:57 +0300

    Update UiRobot Terraform plan - AWS

---
+++ AWS/Terraform/Robots/security-group.tf
@@ -0,0 +1,72 @@
+
+### Security Group Creation ###
+resource "aws_security_group" "uirobot_stack" {
+  name        = "SG-${var.application}-${var.environment}"
+  description = "Security Group for UiRobot_Stack"
+  vpc_id      = "${aws_vpc.uipath.id}"
+
+   tags = {
+    Name = "Security Group - ${var.application}-${var.environment}"
+  }
+
+  # WinRM access from anywhere
+  ingress {
+    from_port   = 5985
+    to_port     = 5986
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    self        = "true"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
