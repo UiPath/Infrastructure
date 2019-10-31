4c10ee461ede81e01b4e89349981b29230578105
Mihai-CMM <costache.mircea.mihai@gmail.com>
2019-10-31 12:10:12 +0200

    Robots on FE

---
+++ OrangeFlexEngine/Terraform/Robots/README.md
@@ -0,0 +1,39 @@
+# Terraform & FE => ♥ UiPath Robot.
+Steps to provision UiPath Robot in AWS :
+1. Install terraform  v0.12.3 (https://learn.hashicorp.com/terraform/getting-started/install.html).
+2. Complete the variables.tf file (see inputs below). For Robots hardware requirements and EC2 types check : https://aws.amazon.com/ec2/instance-types/  and https://robot.uipath.com/docs/hardware-requirements.
+3. Change directory to path of the UiPath Robot plan (cd C:\path\to\uirobot\plan).
+4. Run : ` terraform init `
+5. Run : ` terraform plan `
+6. Check the plan of the resources to be deployed and type ` yes ` if you agree with the plan.
+7. Wait 5-10 mins per EC2 instance and enjoy!
+
+## Terraform version
+Terraform v0.12.12
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|:----:|-----|:----:|:----:|:--:|
+| instance\_count | Number of the Robots to be created. | string | `"2"` | yes |
+| application | Robots and stack prefix name. | string | `"uirobot"` | yes |
+| environment | Environment type, also used as a prefix for the stack. | string | `"prod"` | yes |
+| set\_local\_adminpass | Set local admin password. | string | `"yes"` | yes |
+| admin\_password | Local windows administrator password. If variable 'set_local_adminpass' is 'yes'. | string | `"Local@dminP@55!*"` | yes |
+| cidr\_block | VPC cidr block. | string | `"10.0.0.0/16"` | yes |
+| associate\_public\_ip\_address | Associate public IP to Robots EC2 instances. | string | `"false"` | yes |
+| security\_cidr\_block | Security Group cidr block. | string | `"0.0.0.0/0"` | yes |
+| orchestrator\_url | URL of an existing and licensed Orchestrator. | string | `"https://my-licensed-orchestrator.net"` | yes |
+| tennant | Orchestrator Tennant. | string | `"default"` | yes |
+| robot\_type | Robot type : Attended, Unattended, Development or Nonproduction. | string | `"Unattended"` | yes |
+| api\_user | API user with View, Edit, Create, Delete roles for Machines,Robots and Environments. | string | `"apiUser"` | yes |
+| api\_user\_password | API user password. | string | `"ApiP@ssWd"` | yes |
+| robot\_local\_account | Robot local account which will be created. Don't use 'administrator'. | string | `"robolocal"` | yes |
+| robot\_local\_account\_password | Robot local account password. | string | `"R@pVPsf@Fx"` | yes |
+| robot\_local\_account\_role | Robot local accout role : localadmin or localuser | string | `"localadmin"` | yes |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| bastion\_public\_ip | BastionHost public IP. |
---
+++ OrangeFlexEngine/Terraform/Robots/data-template.tf
@@ -0,0 +1,55 @@
+### INLINE - Bootstrap Windows Server 2016 ###
+data "template_file" "init" {
+  count         = "${var.instance_count}"
+  template = <<EOF
+    <script>
+      winrm quickconfig -q & winrm set winrm/config/winrs @{MaxMemoryPerShellMB="300"} & winrm set winrm/config @{MaxTimeoutms="1800000"} & winrm set winrm/config/service @{AllowUnencrypted="true"} & winrm set winrm/config/service/auth @{Basic="true"} & winrm/config @{MaxEnvelopeSizekb="8000kb"}
+    </script>
+    <powershell>
+    netsh advfirewall firewall add rule name="WinRM in" protocol=TCP dir=in profile=any localport=5985 remoteip=any localip=any action=allow
+
+    ### remove this if you don't want to setup a password for local admin account ###
+    $admin = [ADSI]("WinNT://./administrator, user")
+    $admin.SetPassword("${var.admin_password}")
+    ### end of remove this if you don't want to setup a password for local
+
+    # create Robot local user
+    $robotRole = "${var.robot_local_account_role}"
+    if($robotRole -eq "localuser") {
+      $localRobotRole = "Remote Desktop Users"
+    } else { $localRobotRole = "Administrators" }
+
+    $UserName="${var.robot_local_account}"
+    $Password="${var.robot_local_account_password}"
+
+    $Computer = [ADSI]"WinNT://$Env:COMPUTERNAME,Computer"
+    $User = $Computer.Create("User", $UserName)
+    $User.SetPassword("$Password")
+    $User.SetInfo()
+    $User.FullName = "${var.robot_local_account}"
+    $User.SetInfo()
+    $User.Put("Description", "UiPath Robot Account")
+    $User.SetInfo()
+    $User.UserFlags = 65536
+    $User.SetInfo()
+    $Group = [ADSI]("WinNT://$Env:COMPUTERNAME/$localRobotRole,Group")
+    $Group.add("WinNT://$Env:COMPUTERNAME/$UserName")
+    # end create Robot local user
+
+    $newMachineName = "${var.application}-${var.environment}-${count.index}"
+    Rename-Computer -NewName $newMachineName -Force
+
+    $temp = "C:\Temp"
+    $link = "https://raw.githubusercontent.com/UiPath/Infrastructure/master/Setup/Install-UiRobot.ps1"
+    $file = "Install-UiRobot.ps1"
+    New-Item $temp -ItemType directory
+    New-Item -Path "C:\Temp" -Name "log" -ItemType "directory"
+    Set-Location -Path $temp
+    Set-ExecutionPolicy Unrestricted -force
+    Invoke-WebRequest -Uri $link -OutFile $file
+    & C:\Temp\Install-UiRobot.ps1 -orchestratorUrl '${var.orchestrator_url}' -Tennant '${var.tennant}' -orchAdmin '${var.api_user}' -orchPassword '${var.api_user_password}' -adminUsername '${var.robot_local_account}' -machinePassword '${var.robot_local_account_password}' -HostingType 'Standard' -RobotType '${var.robot_type}' -credType 'Default'
+    Remove-Item -LiteralPath "C:\scripts" -Force -Recurse
+    shutdown /r /f /t 5 /c "forced reboot"
+    </powershell>
+EOF
+}
