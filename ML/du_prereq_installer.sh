82901289bd33eb6441e5819b1abe387ce3b8d0d1
HTeo <hteo1337@gmail.com>
2020-04-07 23:19:13 +0300

    Update du_prereq_installer.sh

--- ML/du_prereq_installer.sh
+++ ML/du_prereq_installer.sh
@@ -7,7 +7,7 @@ distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 VULKAN_FS_REPO_PKG="ftp://ftp.pbone.net/mirror/ftp.scientificlinux.org/linux/scientific/7.0/x86_64/updates/security/vulkan-filesystem-1.1.73.0-1.el7.noarch.rpm"
 VULKAN_FS_PKG="vulkan-filesystem-1.1.73.0-1.el7.noarch.rpm"
 CONTAINER_SELINUX_REPO_PKG="http://mirror.centos.org/centos/7/extras/x86_64/Packages/container-selinux-2.107-1.el7_6.noarch.rpm"
-
+CONTAINERD_IO="https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm"
 
 echo -e "\e[32mUiPath AIFabric Lite"
 tput sgr0
@@ -55,6 +55,27 @@ if [[ -z "$AIF_ENV" ]]; then
     exit 1
 fi
 
+base_prereqs() {
+
+    # pciutils and wget are not installed on some default images ( ex.: AWS marketplace image)
+    if [[ "$distribution"  = "ubuntu"* ]]; then
+        sudo apt-get update > /dev/null 2>&1
+        sudo apt-get upgrade -y > /dev/null 2>&1
+        sudo apt-get install -y pciutils wget > /dev/null 2>&1
+        
+
+    elif  [[ "$distribution"  = "rhel"* ]] || [[ "$distribution"  = "centos"* ]] || [[ "$distribution"  = "amzn"* ]]; then
+        sudo yum update -y  > /dev/null 2>&1
+        sudo yum install -y pciutils wget  > /dev/null 2>&1
+
+    else
+        echo  "Local OS is not supported. Please install latest version of Docker, NVIDIA DRIVERS v430, NVIDIA-Docker and Docker plugin davfs."
+        tput sgr0
+        exit 1
+    fi
+
+}
+
 checking_nvidia_gpu() {
 
     if lspci | grep -i 'nvidia'; then
@@ -75,16 +96,16 @@ install_nvidia_driver() {
           return
     fi
     echo 'blacklist nouveau' >> /etc/modprobe.d/disable-nouveau.conf
-    rmmod nouveau || true 1> /dev/null
+    rmmod nouveau  || true  > /dev/null 2>&1
 
     if [[ "$distribution"  = "ubuntu"* ]]; then
 
             echo -e "\e[32mInstalling NVIDIA DRIVERS"
-            sudo add-apt-repository ppa:graphics-drivers/ppa -y  && \
+            sudo add-apt-repository ppa:graphics-drivers/ppa -y  > /dev/null 2>&1 && \
             sudo apt-get -y update 1> /dev/null && \
             sudo apt-get install linux-headers-$(uname -r) 1> /dev/null && \
             sudo apt-get install -y build-essential gcc-multilib dkms 1> /dev/null && \
-            if [ "$distribution"  = "ubuntu16"* ]; then 
+            if [[ "$distribution"  = "ubuntu16."* ]]; then 
                 sudo apt install -y nvidia-430 1> /dev/null
             else 
                 sudo apt install -y nvidia-driver-430 1> /dev/null
@@ -110,7 +131,7 @@ install_nvidia_driver() {
             tput sgr0
 
     else
-            echo  "Local OS is not supported. Please install latest version of Docker, NVIDIA DRIVERS v430 and Docker plugin uipath davfs"
+            echo  "Local OS is not supported. Please install latest version of Docker, NVIDIA DRIVERS v430 and Docker plugin davfs"
             tput sgr0
             exit 1
     fi
@@ -123,12 +144,12 @@ dockerify_VM() {
     if [[ "$distribution"  = "ubuntu"* ]]; then
 
         echo -e "\e[32mInstalling DOCKER"
-        sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common   && \
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -   && \
-        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"   && \
-        sudo apt-get update   && \
-        sudo apt-get install -y docker-ce   && \
-        sudo usermod -a -G docker $USER   && \
+        sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common  > /dev/null 2>&1  && \
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -  > /dev/null 2>&1  && \
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"  > /dev/null 2>&1  && \
+        sudo apt-get update   > /dev/null 2>&1 && \
+        sudo apt-get install -y docker-ce > /dev/null 2>&1   && \
+        sudo usermod -a -G docker $USER  > /dev/null 2>&1  && \
         echo -e "\e[32m**************DOCKER install SUCCESS! **************" || echo -e "\e[31m--------------DOCKER install FAILED! --------------"
         tput sgr0
         systemctl restart docker && systemctl enable docker && systemctl daemon-reload 
@@ -136,19 +157,22 @@ dockerify_VM() {
     elif [[ "$distribution"  = "rhel"* ]] || [[ "$distribution"  = "centos"* ]] || [[ "$distribution"  = "amzn"* ]]; then
 
         echo -e "\e[32mInstalling DOCKER"
-        sudo yum install -y ${CONTAINER_SELINUX_REPO_PKG}   && \
-        sudo yum install -y selinux-policy container-selinux   && \
-        sudo yum install -y yum-utils device-mapper-persistent-data lvm2   && \
-        sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo   && \
-        sudo yum install -y docker-ce docker-ce-cli containerd.io   && \
-        sudo usermod -a -G docker $USER   && \
+        sudo yum install -y ${CONTAINER_SELINUX_REPO_PKG}  > /dev/null 2>&1  && \
+        sudo yum install -y selinux-policy container-selinux   > /dev/null 2>&1 && \
+        sudo yum install -y yum-utils device-mapper-persistent-data lvm2  > /dev/null 2>&1  && \
+        sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo  > /dev/null 2>&1  && \
+        if [[ "$distribution"  = "rhel8"* ]]; then 
+                sudo yum install -y ${CONTAINERD_IO}   > /dev/null 2>&1 
+        fi  && \
+        sudo yum install -y docker-ce docker-ce-cli containerd.io   > /dev/null 2>&1 && \
+        sudo usermod -a -G docker $USER   > /dev/null 2>&1 && \
         echo -e "\e[32m**************DOCKER install SUCCESS! **************" || echo -e "\e[31m--------------DOCKER install FAILED! --------------"
         tput sgr0
-        systemctl restart docker && systemctl enable docker && systemctl daemon-reload
+        systemctl restart docker && systemctl enable docker && systemctl daemon-reload > /dev/null 2>&1 
 
     else
 
-        echo  "Local OS is not supported. Please install latest version of Docker, NVIDIA DRIVERS v430 and Docker plugin uipath davfs"
+        echo  "Local OS is not supported. Please install latest version of Docker, NVIDIA DRIVERS v430 and Docker plugin davfs"
         tput sgr0
         exit 1
 
@@ -156,13 +180,13 @@ dockerify_VM() {
 
 }
 
-install_uipath_davfs() {
+install_docker_davfs() {
 
-    echo -e "\e[32mInstalling UiPath Docker plugin: davfs"
+    echo -e "\e[32mInstalling Docker plugin: davfs"
     sudo docker plugin disable "uipath/davfs"  > /dev/null 2>&1 && \
     sudo docker plugin rm "uipath/davfs"  > /dev/null 2>&1
     sudo docker plugin install "uipath/davfs" --grant-all-permissions > /dev/null 2>&1 && \
-    echo -e "\e[32m**************UiPath Docker plugin install SUCCESS! **************" || echo -e "\e[31m--------------UiPath Docker plugin install FAILED! --------------"
+    echo -e "\e[32m**************Docker plugin install SUCCESS! **************" || echo -e "\e[31m--------------Docker plugin install FAILED! --------------"
 
 }
 
@@ -178,8 +202,8 @@ test_nvidia_docker() {
 
 
 install_nvidia_docker() {
-
-    if  `whereis nvidia-container-toolkit > /dev/null 2>&1` || `whereis nvidia-container-runtime > /dev/null 2>&1`; then
+    # `whereis nvidia-container-toolkit > /dev/null 2>&1` || 
+    if  `which nvidia-container-runtime > /dev/null 2>&1`; then
           echo -e "\e[32m**************NVIDIA Container runtime is present. **************"
           tput sgr0
           return
@@ -188,10 +212,13 @@ install_nvidia_docker() {
     if [[ "$distribution"  = "ubuntu"* ]]; then
 
         echo -e "\e[32mInstalling NVIDIA Container runtime"
-        curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -   && \
-        curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list   && \
-        sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit   && \
-        sudo systemctl restart docker   && \
+        # Update repo keys for Debian-based distros
+        curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | sudo apt-key add - > /dev/null 2>&1  && \
+        # curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -   > /dev/null 2>&1  && \
+        #curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list   && \
+        curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list  > /dev/null 2>&1 && \
+        sudo apt-get update && sudo apt-get install -y nvidia-container-runtime  > /dev/null 2>&1   && \
+        sudo systemctl restart docker   > /dev/null 2>&1  && \
         echo -e "\e[32m**************NVIDIA Container runtime install SUCCESS! **************" || echo -e "\e[31m--------------NVIDIA Container runtime install FAILED! --------------"
         tput sgr0
         # Test if Nvidia-Docker works
@@ -201,7 +228,13 @@ install_nvidia_docker() {
     elif  [[ "$distribution"  = "rhel"* ]] || [[ "$distribution"  = "centos"* ]] || [[ "$distribution"  = "amzn"* ]]; then
         
         echo -e "\e[32mInstalling NVIDIA Container runtime"
-        curl -s -L https://nvidia.github.io/nvidia-container-runtime/rhel7.5/nvidia-container-runtime.repo | sudo tee /etc/yum.repos.d/nvidia-container-runtime.repo
+        # Update repo keys for RHEL-based distros
+        DIST=$(sed -n 's/releasever=//p' /etc/yum.conf)
+        DIST=${DIST:-$(. /etc/os-release; echo $VERSION_ID)}
+        sudo rpm -e gpg-pubkey-f796ecb0 > /dev/null 2>&1
+        sudo gpg --homedir /var/lib/yum/repos/$(uname -m)/$DIST/nvidia-container-runtime/gpgdir --delete-key f796ecb0 > /dev/null 2>&1
+        sudo yum makecache > /dev/null 2>&1
+        curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.repo | sudo tee /etc/yum.repos.d/nvidia-container-runtime.repo  > /dev/null 2>&1 && \
         sudo yum install -y nvidia-container-runtime
         sudo systemctl restart docker
         echo -e "\e[32m**************NVIDIA Container runtime install SUCCESS! **************" || echo -e "\e[31m--------------NVIDIA Container runtime install FAILED! --------------"
@@ -212,7 +245,7 @@ install_nvidia_docker() {
         tput sgr0
 
     else
-        echo  "Local OS is not supported. Please install latest version of Docker, NVIDIA DRIVERS v430, NVIDIA-Docker and Docker plugin uipath davfs."
+        echo  "Local OS is not supported. Please install latest version of Docker, NVIDIA DRIVERS v430, NVIDIA-Docker and Docker plugin davfs."
         tput sgr0
         exit 1
     fi
@@ -237,15 +270,16 @@ install_docker() {
 Main() {
 
     if [[ "$AIF_ENV" == "serving" ]]; then
+        base_prereqs
         install_docker
-        install_uipath_davfs
+        install_docker_davfs
 
     elif [[ "$AIF_ENV" == "training" ]]; then
-
+        base_prereqs
         checking_nvidia_gpu
         install_nvidia_driver
         install_docker
-        install_uipath_davfs
+        install_docker_davfs
         install_nvidia_docker
         
     else
